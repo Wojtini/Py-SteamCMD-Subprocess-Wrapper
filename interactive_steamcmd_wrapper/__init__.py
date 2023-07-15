98fcfa89cc1d4d7bf5c42363de156222a1086b0c
Wojtini <whitem200@gmail.com>
2023-07-15 17:49:15 +0200

    SteamCMD new warning handling clean up and cleanup

--- interactive_steamcmd_wrapper/__init__.py
+++ interactive_steamcmd_wrapper/__init__.py
@@ -1,6 +1,36 @@
-from interactive_steamcmd_wrapper.exceptions import ISteamCMDProcessError, ISteamCMDAlreadyInstalled
-from interactive_steamcmd_wrapper.steamcmd_process import ISteamCMDProcess
-from interactive_steamcmd_wrapper.steamcmd_wrapper import InteractiveSteamCMD
+import logging
+from pathlib import Path
+
+from interactive_steamcmd_wrapper.steam_client.steamcmd_client import SteamCMDClient
+from interactive_steamcmd_wrapper.steam_downloader.downloader_requests import DownloadRequests
+from interactive_steamcmd_wrapper.steam_downloader.file_system import FileSystem
+from interactive_steamcmd_wrapper.steam_downloader.linux_steamcmd_downloader import (
+    LinuxSteamCMDDownloader,
+)
+from interactive_steamcmd_wrapper.steam_subprocess.linux_steam_subprocess import (
+    LinuxSteamCMDSubprocess,
+)
 
 
-__all__ = ["InteractiveSteamCMD", "ISteamCMDAlreadyInstalled", "ISteamCMDProcessError", "ISteamCMDProcess"]
+class UnsupportedPlatformError(Exception):
+    def __init__(self, system_name: str) -> None:
+        super().__init__(f"{system_name} is unsupported platform")
+
+
+def get_steamcmd_client(path_location: str) -> SteamCMDClient:
+    logging.basicConfig(level=logging.DEBUG)
+
+    filesystem = FileSystem()
+    LinuxSteamCMDDownloader(
+        path_location,
+        DownloadRequests(),
+        filesystem,
+    ).download()
+    subprocess = LinuxSteamCMDSubprocess(Path(str(path_location) + "/steamcmd.sh"))
+    return SteamCMDClient(
+        subprocess,
+        filesystem,
+    )
+
+
+__all__ = ["get_steamcmd_client", ""]
